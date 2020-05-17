51c48232bca42f12692a8eadee5c019603158364
yumene.takeda <yuyuyukoppepan@gmail.com>
2020-05-17 14:43:53 +0000

    feat:post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
           redirect_to login_url
         end
     end
+    
+    def counts(user)
+        @cout_microposts = user.microposts.count
+    end
 end
