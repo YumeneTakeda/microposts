e58ba22fab4a03f90b53d32a8192b32cdb6096ff
yumene.takeda <yuyuyukoppepan@gmail.com>
2020-05-28 12:55:10 +0000

    feat:add follow,unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,7 @@ class ApplicationController < ActionController::Base
     
     def counts(user)
         @cout_microposts = user.microposts.count
+        @count_followings = user.followings.count
+        @count_followers = user.followers.count
     end
 end
